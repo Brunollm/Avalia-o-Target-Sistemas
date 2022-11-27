3dfefd52ddfa34ac12f62740e96173d124bc48bc
Brunollm <110358647+Brunollm@users.noreply.github.com>
2022-11-26 23:23:26 -0300

    Add files via upload

---
+++ targetex05.cs
@@ -0,0 +1,12 @@
+﻿string stringOriginal = "abc";
+string stringReversa = "";
+int tamanhoArray;
+
+tamanhoArray = stringOriginal.Length - 1;
+
+while (tamanhoArray >= 0)
+{
+    stringReversa = stringReversa + stringOriginal[tamanhoArray];
+    tamanhoArray--;
+}
+Console.WriteLine(stringReversa);
