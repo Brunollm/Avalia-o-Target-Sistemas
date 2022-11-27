af75f52900e0b4db3859c8aea1d6b2ce0395cfc7
Brunollm <110358647+Brunollm@users.noreply.github.com>
2022-11-27 00:05:10 -0300

    Add files via upload

---
+++ targetex04.cs
@@ -0,0 +1,12 @@
+﻿float totalSoma;
+float[] cidade = new float[] { 67836.43f, 36678.66f, 29229.88f, 27165.48f, 19849.53f };
+string[] nomeCidade = new string[] { "São Paulo", "Rio de Janeiro", "Minas Gerais", "Espírito Santo", "Outros" };
+
+totalSoma = cidade[0] + cidade[1] + cidade[2] + cidade[3] + cidade[4];
+
+Console.WriteLine("Total da Soma de todas as cidades: " + totalSoma);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Valor percentual na cidade de " + nomeCidade[i] + " é de: " + ((cidade[i] * 100) / totalSoma).ToString("#.##") + "%");
+}
